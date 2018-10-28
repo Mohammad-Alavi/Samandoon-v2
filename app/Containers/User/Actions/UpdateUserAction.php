50808b7465bcce1cfed85be26da045f2e323fdc1
Moslem Deris <moslem.deris@gmail.com>
2018-10-28 17:20:21 +0330

    Correct updateable fields

--- app/Containers/User/Actions/UpdateUserAction.php
+++ app/Containers/User/Actions/UpdateUserAction.php
@@ -6,33 +6,21 @@ use Apiato\Core\Foundation\Facades\Apiato;
 use App\Containers\User\Models\User;
 use App\Ship\Parents\Actions\Action;
 use App\Ship\Transporters\DataTransporter;
-use Illuminate\Support\Facades\Hash;
 
-/**
- * Class UpdateUserAction.
- *
- * @author Mahmoud Zalt <mahmoud@zalt.me>
- */
-class UpdateUserAction extends Action
-{
+class UpdateUserAction extends Action {
 
     /**
      * @param \App\Ship\Transporters\DataTransporter $data
      *
      * @return  \App\Containers\User\Models\User
      */
-    public function run(DataTransporter $data): User
-    {
+    public function run(DataTransporter $data): User {
         $userData = [
-            'password'             => $data->password ? Hash::make($data->password) : null,
-            'name'                 => $data->name,
-            'email'                => $data->email,
-            'gender'               => $data->gender,
-            'birth'                => $data->birth,
-            'social_token'         => $data->token,
-            'social_expires_in'    => $data->expiresIn,
-            'social_refresh_token' => $data->refreshToken,
-            'social_token_secret'  => $data->tokenSecret,
+            'first_name' => $data->first_name,
+            'last_name'  => $data->last_name,
+            'email'      => $data->email,
+            'gender'     => $data->gender,
+            'birth'      => $data->birth
         ];
 
         // remove null values and their keys
