222eec1a135e6a6b7f820f61c87a48bec149a113
Moslem Deris <moslem.deris@gmail.com>
2018-11-06 01:06:07 +0330

    Remove container `SocialAuth`

--- app/Containers/SocialAuth/UI/WEB/Routes/GetProvider.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// provider login redirect (WEB)
-$router->get('auth/{provider}', [
-    'as' => 'web_socialauth_redirect',
-    'uses' => 'Controller@redirectAll',
-]);
--- app/Containers/SocialAuth/UI/WEB/Routes/ProviderCallback.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// provider callback handler
-$router->any('auth/{provider}/callback', [
-    'as' => 'web_socialauth_callback',
-    'uses' => 'Controller@handleCallbackAll',
-]);
-
