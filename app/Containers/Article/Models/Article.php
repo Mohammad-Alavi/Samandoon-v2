d695febd7c5fea7bb1f400c8006e39667a702e46
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-11-11 03:07:59 +0330

    Create Article model

---
+++ app/Containers/Article/Models/Article.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Containers\Article\Models;
+
+use App\Ship\Parents\Models\Model;
+
+class Article extends Model
+{
+    protected $fillable = [
+        'title',
+        'text',
+    ];
+
+    protected $attributes = [
+
+    ];
+
+    protected $hidden = [
+
+    ];
+
+    protected $casts = [
+
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * A resource key to be used by the the JSON API Serializer responses.
+     */
+    protected $resourceKey = 'articles';
+}
