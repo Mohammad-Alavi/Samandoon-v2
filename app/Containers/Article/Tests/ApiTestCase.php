ac608d3fcac29b0a2bff0d247b3dff218ef2045f
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-11-15 01:23:43 +0330

    Add some files for setting up API Functional tests

---
+++ app/Containers/Article/Tests/ApiTestCase.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Containers\Article\Tests;
+
+use App\Containers\Article\Tests\TestCase as BaseTestCase;
+
+/**
+ * Class ApiTestCase.
+ *
+ * This is the container API TestCase class. Use this class to add your container specific API related helper functions.
+ */
+class ApiTestCase extends BaseTestCase
+{
+    // ..
+}
