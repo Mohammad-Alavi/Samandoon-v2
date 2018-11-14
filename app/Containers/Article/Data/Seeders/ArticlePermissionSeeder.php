81a8ae2f8441c8c9c696b8827e1567da61095ee8
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-11-15 01:20:41 +0330

    Add Article permission seeder

---
+++ app/Containers/Article/Data/Seeders/ArticlePermissionSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Containers\Article\Data\Seeders;
+
+use Apiato\Core\Foundation\Facades\Apiato;
+use App\Ship\Parents\Seeders\Seeder;
+
+class ArticlePermissionSeeder extends Seeder
+{
+    /**
+     * @return void
+     */
+    public function run()
+    {
+        // Default Permissions ----------------------------------------------------------
+        Apiato::call('Authorization@CreatePermissionTask', ['create-article', 'Create Article']);
+        Apiato::call('Authorization@CreatePermissionTask', ['update-article', 'Create Article']);
+        Apiato::call('Authorization@CreatePermissionTask', ['delete-article', 'Create Article']);
+    }
+}
