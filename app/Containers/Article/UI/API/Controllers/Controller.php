92e15e06905134581b3f289581be3c4ac626f720
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-11-15 01:19:39 +0330

    Add Controller and its methods

--- app/Containers/Article/UI/API/Controllers/Controller.php
+++ app/Containers/Article/UI/API/Controllers/Controller.php
@@ -2,9 +2,31 @@
 
 namespace App\Containers\Article\UI\API\Controllers;
 
+use Apiato\Core\Foundation\Facades\Apiato;
+use App\Containers\Article\UI\API\Requests\CreateArticleRequest;
+use App\Containers\Article\UI\API\Requests\GetArticleRequest;
+use App\Containers\Article\UI\API\Requests\UpdateArticleRequest;
+use App\Containers\Article\UI\API\Transformers\ArticleTransformer;
 use App\Ship\Parents\Controllers\ApiController;
+use App\Ship\Transporters\DataTransporter;
 
 class Controller extends ApiController
 {
+    public function createArticle(CreateArticleRequest $request)
+    {
+        $article = Apiato::call('Article@CreateArticleAction', new DataTransporter([$request]));
+        return $this->transform($article, ArticleTransformer::class);
+    }
 
+    public function updateArticle(UpdateArticleRequest $request)
+    {
+        $article = Apiato::call('Article@UpdateArticleAction', new DataTransporter([$request]));
+        return $this->transform($article, ArticleTransformer::class);
+    }
+
+    public function getArticle(GetArticleRequest $request)
+    {
+        $article = Apiato::call('Article@CreateArticleAction', new DataTransporter([$request]));
+        return $this->transform($article, ArticleTransformer::class);
+    }
 }
