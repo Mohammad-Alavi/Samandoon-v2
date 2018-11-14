d91433d766efdb178bfb82154d547d5b25b242bc
Moslem Deris <moslem.deris@gmail.com>
2018-11-14 16:44:07 +0330

    Fix a bug in TestCase

--- app/Containers/User/Tests/TestCase.php
+++ app/Containers/User/Tests/TestCase.php
@@ -2,21 +2,26 @@
 
 namespace App\Containers\User\Tests;
 
-use App\Containers\User\Actions\RegisterUserByPhoneSubAction;
+use App\Containers\User\Actions\CreateUserByPhoneAndPasswordSubAction;
 use App\Containers\User\Models\User;
 use App\Containers\User\Tasks\DeleteAllUsersTask;
 use App\Ship\Parents\Tests\PhpUnit\TestCase as ShipTestCase;
+use App\Ship\Transporters\DataTransporter;
 use Illuminate\Support\Facades\App;
 
 class TestCase extends ShipTestCase {
 
     /**
      * @param string $phone
+     * @param string $password
+     *
      * @return  User
      */
-    public function getNewUser(string $phone = '+98916-------'): User {
-        $action = App::make(RegisterUserByPhoneSubAction::class);
-        $user = $action->run($phone);
+    public function getNewUser(string $phone = '+98916-------', string $password = 'password'): User {
+        $action = App::make(CreateUserByPhoneAndPasswordSubAction::class);
+        $transporter = new DataTransporter(['phone' => $phone, 'password' => $password]);
+        $user = $action->run($transporter);
+
         return $user;
     }
 
@@ -24,12 +29,12 @@ class TestCase extends ShipTestCase {
      * @param int $count
      */
     public function getNewUsers(int $count) {
-        for ($i = 0; $i <$count; $i++){
+        for ($i = 0; $i < $count; $i++) {
             $this->getNewUser('+98936---' . $i);
         }
     }
 
-    public function deleteAllUsers(){
+    public function deleteAllUsers() {
         App::make(DeleteAllUsersTask::class)->run();
     }
 
