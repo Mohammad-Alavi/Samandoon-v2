c6f23292f03384728aa145d3bd5f4a23d57923b1
Moslem Deris <moslem.deris@gmail.com>
2018-11-15 23:54:56 +0330

    Modify class to use DI and be testable

--- app/Containers/User/Actions/CreateAdminByPhoneAndPasswordSubAction.php
+++ app/Containers/User/Actions/CreateAdminByPhoneAndPasswordSubAction.php
@@ -2,18 +2,58 @@
 
 namespace App\Containers\User\Actions;
 
-use Apiato\Core\Foundation\Facades\Apiato;
+use App\Containers\Authorization\Tasks\AssignRoleToUserTask;
 use App\Containers\User\Exceptions\PhoneIsExistingException;
 use App\Containers\User\Models\User;
+use App\Containers\User\Tasks\CheckIfPhoneIsExistingTask;
+use App\Containers\User\Tasks\CreateUserByPhoneTask;
 use App\Ship\Parents\Actions\SubAction;
 use App\Ship\Transporters\DataTransporter;
 
 class CreateAdminByPhoneAndPasswordSubAction extends SubAction {
 
     /**
+     * @var CheckIfPhoneIsExistingTask
+     */
+    private $checkIfPhoneIsExistingTask;
+
+    /**
+     * @var CreateUserByPhoneTask
+     */
+    private $createUserByPhoneTask;
+
+    /**
+     * @var UpdateUserPasswordSubAction
+     */
+    private $updateUserPasswordSubAction;
+
+    /**
+     * @var AssignRoleToUserTask
+     */
+    private $assignRoleToUserTask;
+
+    /**
      * @var User
      */
     private $user;
+
+    /**
+     * CreateAdminByPhoneAndPasswordSubAction constructor.
+     *
+     * @param CheckIfPhoneIsExistingTask  $checkIfPhoneIsExistingTask
+     * @param CreateUserByPhoneTask       $createUserByPhoneTask
+     * @param UpdateUserPasswordSubAction $updateUserPasswordSubAction
+     * @param AssignRoleToUserTask        $assignRoleToUserTask
+     */
+    public function __construct(CheckIfPhoneIsExistingTask $checkIfPhoneIsExistingTask,
+                                CreateUserByPhoneTask $createUserByPhoneTask,
+                                UpdateUserPasswordSubAction $updateUserPasswordSubAction,
+                                AssignRoleToUserTask $assignRoleToUserTask) {
+        $this->checkIfPhoneIsExistingTask = $checkIfPhoneIsExistingTask;
+        $this->createUserByPhoneTask = $createUserByPhoneTask;
+        $this->updateUserPasswordSubAction = $updateUserPasswordSubAction;
+        $this->assignRoleToUserTask = $assignRoleToUserTask;
+    }
 
     /**
      * @param DataTransporter $data
@@ -23,19 +63,19 @@ class CreateAdminByPhoneAndPasswordSubAction extends SubAction {
      */
     public function run(DataTransporter $data): User {
         //  Check if user had been registered before.
-        $isPhoneExisting = Apiato::call('User@CheckIfPhoneIsExistingTask', [$data->phone]);
+        $isPhoneExisting = $this->checkIfPhoneIsExistingTask->run($data->phone);
 
         //  Error if user exists
         throw_if($isPhoneExisting, new PhoneIsExistingException());
 
         //  Create user
-        $this->user = Apiato::call('User@CreateUserByPhoneTask', [false, $data->phone]);
+        $this->user = $this->createUserByPhoneTask->run(false, $data->phone);
 
         //  Set the password
-        $this->user = Apiato::call('User@UpdateUserPasswordSubAction', [$this->user->id, $data->password]);
+        $this->user = $this->updateUserPasswordSubAction->run($this->user->id, $data->password);
 
         //  Assign Roles to admin
-        Apiato::call('Authorization@AssignRoleToUserTask', [$this->user, ['admin']]);
+        $this->assignRoleToUserTask->run($this->user, ['admin']);
 
         return $this->user;
     }
