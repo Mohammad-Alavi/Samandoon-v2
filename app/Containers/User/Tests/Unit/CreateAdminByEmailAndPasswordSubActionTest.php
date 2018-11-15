2244566c977ef1a17702987f5b2f0faf607b24b6
Moslem Deris <moslem.deris@gmail.com>
2018-11-16 01:23:46 +0330

    Add test files

---
+++ app/Containers/User/Tests/Unit/CreateAdminByEmailAndPasswordSubActionTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Containers\User\Tests\Unit;
+
+use App\Containers\Authorization\Tasks\AssignRoleToUserTask;
+use App\Containers\User\Actions\CreateAdminByEmailAndPasswordSubAction;
+use App\Containers\User\Actions\UpdateUserPasswordSubAction;
+use App\Containers\User\Exceptions\EmailIsExistingException;
+use App\Containers\User\Models\User;
+use App\Containers\User\Tasks\CheckIfEmailIsExistingTask;
+use App\Containers\User\Tasks\CreateUserByEmailTask;
+use App\Containers\User\Tests\TestCase;
+use App\Ship\Transporters\DataTransporter;
+use PHPUnit\Framework\MockObject\MockObject;
+
+class CreateAdminByEmailAndPasswordSubActionTest extends TestCase {
+
+    /**
+     * @var MockObject | CheckIfEmailIsExistingTask
+     */
+    private $checkIfEmailIsExistingTask;
+
+    /**
+     * @var MockObject | CreateUserByEmailTask
+     */
+    private $createUserByEmailTask;
+
+    /**
+     * @var MockObject | UpdateUserPasswordSubAction
+     */
+    private $updateUserPasswordSubAction;
+
+    /**
+     * @var MockObject | AssignRoleToUserTask
+     */
+    private $assignRoleToUserTask;
+
+    public function setUp() {
+        parent::setUp();
+
+        $this->checkIfEmailIsExistingTask = $this->getMockBuilder(CheckIfEmailIsExistingTask::class)
+            ->disableOriginalConstructor()->getMock();
+        $this->createUserByEmailTask = $this->getMockBuilder(CreateUserByEmailTask::class)
+            ->disableOriginalConstructor()->getMock();
+        $this->updateUserPasswordSubAction = $this->getMockBuilder(UpdateUserPasswordSubAction::class)
+            ->getMock();
+        $this->assignRoleToUserTask = $this->getMockBuilder(AssignRoleToUserTask::class)
+            ->getMock();
+
+    }
+
+    public function test_ShouldReturnUser() {
+        $email = 'moslem.deris@test.ir';
+        $password = 'secret';
+        $admin = $this->createUserByEmail($email, $password, false);
+
+        $this->checkIfEmailIsExistingTask->method('run')->will($this->returnValue(false));
+        $this->createUserByEmailTask->method('run')->will($this->returnValue($admin));
+        $this->updateUserPasswordSubAction->method('run')->will($this->returnValue($admin));
+        $this->assignRoleToUserTask->method('run')->will($this->returnValue($admin));
+
+        $subAction = new CreateAdminByEmailAndPasswordSubAction(
+            $this->checkIfEmailIsExistingTask,
+            $this->createUserByEmailTask,
+            $this->updateUserPasswordSubAction,
+            $this->assignRoleToUserTask);
+
+        $transporter = new DataTransporter(['email' => $email, 'password' => $password]);
+        $admin = $subAction->run($transporter);
+
+        $this->assertInstanceOf(User::class, $admin);
+    }
+
+    public function test_ShouldThrowExceptionWhenEmailExists() {
+        $email = 'moslem.deris@test.ir';
+        $password = 'secret';
+        $admin = $this->createUserByEmail($email, $password, false);
+
+        $this->checkIfEmailIsExistingTask->method('run')->will($this->returnValue(true));
+        $this->createUserByEmailTask->method('run')->will($this->returnValue($admin));
+        $this->updateUserPasswordSubAction->method('run')->will($this->returnValue($admin));
+        $this->assignRoleToUserTask->method('run')->will($this->returnValue($admin));
+
+        $subAction = new CreateAdminByEmailAndPasswordSubAction(
+            $this->checkIfEmailIsExistingTask,
+            $this->createUserByEmailTask,
+            $this->updateUserPasswordSubAction,
+            $this->assignRoleToUserTask);
+
+        $this->expectException(EmailIsExistingException::class);
+
+        $transporter = new DataTransporter(['email' => $email, 'password' => $password]);
+        $admin = $subAction->run($transporter);
+    }
+}
---
+++ app/Containers/User/Tests/Unit/CreateUserByEmailAndPasswordSubActionTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Containers\User\Tests\Unit;
+
+use App\Containers\User\Actions\CreateUserByEmailAndPasswordSubAction;
+use App\Containers\User\Actions\UpdateUserPasswordSubAction;
+use App\Containers\User\Exceptions\EmailIsExistingException;
+use App\Containers\User\Models\User;
+use App\Containers\User\Tasks\CheckIfEmailIsExistingTask;
+use App\Containers\User\Tasks\CreateUserByEmailTask;
+use App\Containers\User\Tests\TestCase;
+use App\Ship\Transporters\DataTransporter;
+use PHPUnit\Framework\MockObject\MockObject;
+
+class CreateUserByEmailAndPasswordSubActionTest extends TestCase {
+
+    /**
+     * @var MockObject | CheckIfEmailIsExistingTask
+     */
+    private $checkIfEmailIsExistingTask;
+
+    /**
+     * @var MockObject | CreateUserByEmailTask
+     */
+    private $createUserByEmailTask;
+
+    /**
+     * @var MockObject | UpdateUserPasswordSubAction
+     */
+    private $updateUserPasswordSubAction;
+
+    public function setUp() {
+        parent::setUp();
+
+        $this->checkIfEmailIsExistingTask = $this->getMockBuilder(CheckIfEmailIsExistingTask::class)
+            ->disableOriginalConstructor()->getMock();
+        $this->createUserByEmailTask = $this->getMockBuilder(CreateUserByEmailTask::class)
+            ->disableOriginalConstructor()->getMock();
+        $this->updateUserPasswordSubAction = $this->getMockBuilder(UpdateUserPasswordSubAction::class)
+            ->getMock();
+    }
+
+    public function test_ShouldReturnUser() {
+        $email = 'moslem.deris@test.ir';
+        $password = 'secret';
+        $user = $this->createUserByEmail($email, $password);
+
+        $this->checkIfEmailIsExistingTask->method('run')->will($this->returnValue(false));
+        $this->createUserByEmailTask->method('run')->will($this->returnValue($user));
+        $this->updateUserPasswordSubAction->method('run')->will($this->returnValue($user));
+
+        $subAction = new CreateUserByEmailAndPasswordSubAction(
+            $this->checkIfEmailIsExistingTask,
+            $this->createUserByEmailTask,
+            $this->updateUserPasswordSubAction);
+
+        $transporter = new DataTransporter(['email' => $email, 'password' => $password]);
+        $admin = $subAction->run($transporter);
+
+        $this->assertInstanceOf(User::class, $admin);
+    }
+
+    public function test_ShouldThrowExceptionWhenEmailExists() {
+        $email = 'moslem.deris@test.ir';
+        $password = 'secret';
+        $user = $this->createUserByEmail($email, $password);
+
+        $this->checkIfEmailIsExistingTask->method('run')->will($this->returnValue(true));
+        $this->createUserByEmailTask->method('run')->will($this->returnValue($user));
+        $this->updateUserPasswordSubAction->method('run')->will($this->returnValue($user));
+
+        $subAction = new CreateUserByEmailAndPasswordSubAction(
+            $this->checkIfEmailIsExistingTask,
+            $this->createUserByEmailTask,
+            $this->updateUserPasswordSubAction);
+
+        $this->expectException(EmailIsExistingException::class);
+
+        $transporter = new DataTransporter(['email' => $email, 'password' => $password]);
+        $admin = $subAction->run($transporter);
+    }
+}
