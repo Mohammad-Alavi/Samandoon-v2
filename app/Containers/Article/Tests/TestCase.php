8c20d3008a7b813507d6fb10c1a46f470989dc7d
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-11-16 04:02:51 +0330

    Removed unused variables and methods

--- app/Containers/Article/Tests/TestCase.php
+++ app/Containers/Article/Tests/TestCase.php
@@ -2,10 +2,7 @@
 
 namespace App\Containers\Article\Tests;
 
-use App\Containers\Article\Models\Article;
 use App\Ship\Parents\Tests\PhpUnit\TestCase as ShipTestCase;
-use App\Ship\Transporters\DataTransporter;
-use phpDocumentor\Reflection\Types\Integer;
 
 /**
  * Class TestCase.
@@ -14,40 +11,4 @@ use phpDocumentor\Reflection\Types\Integer;
  */
 class TestCase extends ShipTestCase
 {
-    /**@var  array $data */
-    protected $data;
-    /**@var DataTransporter $transporter */
-    protected $transporter;
-    /** @var Article $article */
-    protected $article;
-
-    public function setUp()
-    {
-        parent::setUp();
-        // create a data object
-        $this->data = [
-            'title' => $this->faker->realText(80),
-            'text' => $this->faker->realText(200),
-        ];
-
-        $this->transporter = new DataTransporter($this->data);
-    }
-
-    /**
-     * @return Article
-     */
-    public function createNewArticle()
-    {
-        $article = new Article($this->data);
-        $article->save();
-        return $article;
-    }
-
-    public function tearDown()
-    {
-        parent::tearDown();
-        unset($this->data);
-        unset($this->transporter);
-        unset($this->article);
-    }
 }
