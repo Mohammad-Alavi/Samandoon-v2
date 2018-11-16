13a4589d2af3ca723d9f272e329a14ed1657f665
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-11-16 04:07:11 +0330

    Now use DI and Mock for tests

--- app/Containers/Article/Tests/Unit/GetArticleActionTest.php
+++ app/Containers/Article/Tests/Unit/GetArticleActionTest.php
@@ -4,9 +4,11 @@ namespace App\Containers\Article\Tests\Unit;
 
 use App\Containers\Article\Actions\GetArticleAction;
 use App\Containers\Article\Models\Article;
+use App\Containers\Article\Tasks\FindArticleByIdTask;
 use App\Containers\Article\Tests\TestCase;
 use App\Ship\Transporters\DataTransporter;
 use Illuminate\Support\Facades\App;
+use PHPUnit\Framework\MockObject\MockObject;
 
 /**
  * Class FindArticleByIdActionTest.
@@ -20,28 +22,47 @@ class GetArticleActionTest extends TestCase
     private $action;
     /** @var Article $foundArticle */
     private $foundArticle;
+    /** @var Article $article */
+    private $article;
+    /** @var array $data */
+    private $data;
+    /** @var MockObject|FindArticleByIdTask $findArticleByIdTask */
+    private $findArticleByIdTask;
+    /** @var DataTransporter $transporter */
+    private $transporter;
 
     public function setUp()
     {
         parent::setUp();
-        $this->article = $this->createNewArticle();
 
         $this->data = [
-            'id' => $this->article->id,
+            'title' => 'این یک تایتل زیبا در مورد یک نوشته زیباست',
+            'text' => 'این یک تایتل زیبا در مورد یک نوشته زیباست این یک تایتل زیبا در مورد یک نوشته زیباست این یک تایتل زیبا در مورد یک نوشته زیباست این یک تایتل زیبا در مورد یک نوشته زیباست این یک تایتل زیبا در مورد یک نوشته زیباست این یک تایتل زیبا در مورد یک نوشته زیباست این یک تایتل زیبا در مورد یک نوشته زیباست این یک تایتل زیبا در مورد یک نوشته زیباست این یک تایتل زیبا در مورد یک نوشته زیباست این یک تایتل زیبا در مورد یک نوشته زیباست',
         ];
+        // Create a new article with the provided data and save it into the database
+        $this->article = new Article($this->data);
+        $this->article->save();
 
-        $this->action = App::make(GetArticleAction::class);
-        $this->transporter = new DataTransporter($this->data);
-        $this->foundArticle = $this->action->run($this->transporter);
+        $this->findArticleByIdTask = $this->getMockBuilder(FindArticleByIdTask::class)
+            ->setMethods(['run'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->action = new GetArticleAction($this->findArticleByIdTask);
+        $this->transporter = new DataTransporter(['id' => $this->article->id]);
     }
 
     public function test_FindArticleByIdAction()
     {
+        $this->findArticleByIdTask->expects($this->once())
+            ->method('run')
+            ->with($this->article->id)
+            ->willReturn($this->article);
+        $this->foundArticle = $this->action->run($this->transporter);
+
         $this->assertInstanceOf(Article::class, $this->article, 'The returned object is not an instance of the Article.');
 
-        $this->assertEquals($this->article->id, $this->foundArticle->id);
-        $this->assertEquals($this->article->title, $this->foundArticle->title);
-        $this->assertEquals($this->article->text, $this->foundArticle->text);
+        $this->assertEquals($this->article, $this->foundArticle);
     }
 
     public function tearDown()
