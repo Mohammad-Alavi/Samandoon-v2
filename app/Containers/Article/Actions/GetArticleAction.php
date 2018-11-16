35e281741e42e7510d7cd34d2bb225789ee5fb21
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-11-16 04:05:55 +0330

    Refactor to use ID

--- app/Containers/Article/Actions/GetArticleAction.php
+++ app/Containers/Article/Actions/GetArticleAction.php
@@ -2,21 +2,29 @@
 
 namespace App\Containers\Article\Actions;
 
-use Apiato\Core\Foundation\Facades\Apiato;
 use App\Containers\Article\Models\Article;
+use App\Containers\Article\Tasks\FindArticleByIdTask;
 use App\Ship\Parents\Actions\Action;
 use App\Ship\Transporters\DataTransporter;
 
-class GetArticleAction extends Action {
+class GetArticleAction extends Action
+{
+
+    protected $findArticleByIdTask;
+
+    public function __construct(FindArticleByIdTask $findArticleByIdTask)
+    {
+        $this->findArticleByIdTask = $findArticleByIdTask;
+    }
 
     /**
      * @param DataTransporter $transporter
      *
      * @return Article
      */
-    public function run(DataTransporter $transporter): Article {
-        $article = Apiato::call('Article@FindArticleByIdTask', [$transporter->id]);
-
+    public function run(DataTransporter $transporter): Article
+    {
+        $article = $this->findArticleByIdTask->run($transporter->id);
         return $article;
     }
 }
