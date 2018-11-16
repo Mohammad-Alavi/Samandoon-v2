70b13df8a039732e466ea2c4224d4596f31cf117
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-11-16 04:04:00 +0330

    Add DI

--- app/Containers/Article/Actions/CreateArticleAction.php
+++ app/Containers/Article/Actions/CreateArticleAction.php
@@ -2,25 +2,34 @@
 
 namespace App\Containers\Article\Actions;
 
-use Apiato\Core\Foundation\Facades\Apiato;
 use App\Containers\Article\Models\Article;
+use App\Containers\Article\Tasks\CreateArticleTask;
 use App\Ship\Parents\Actions\Action;
 use App\Ship\Transporters\DataTransporter;
 
-class CreateArticleAction extends Action {
+class CreateArticleAction extends Action
+{
+    /** @var CreateArticleTask $createArticleTask */
+    protected $createArticleTask;
+
+    public function __construct(CreateArticleTask $createArticleTask)
+    {
+        $this->createArticleTask = $createArticleTask;
+    }
 
     /**
      * @param DataTransporter $transporter
      *
      * @return Article
      */
-    public function run(DataTransporter $transporter): Article {
+    public function run(DataTransporter $transporter): Article
+    {
         $data = $transporter->sanitizeInput([
             'title',
             'text',
         ]);
 
-        $article = Apiato::call('Article@CreateArticleTask', [$data]);
+        $article = $this->createArticleTask->run($data);
 
         return $article;
     }
