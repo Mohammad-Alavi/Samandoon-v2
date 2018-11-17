3aca76a956f8e35b4deafe5e6c15eea63473f700
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-11-18 01:52:43 +0330

    Add some shared functioanilty

--- app/Containers/Article/Tests/TestCase.php
+++ app/Containers/Article/Tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace App\Containers\Article\Tests;
 
+use App\Containers\Article\Models\Article;
 use App\Ship\Parents\Tests\PhpUnit\TestCase as ShipTestCase;
 
 /**
@@ -11,4 +12,21 @@ use App\Ship\Parents\Tests\PhpUnit\TestCase as ShipTestCase;
  */
 class TestCase extends ShipTestCase
 {
+    protected const RAW_ARTICLE_DATA = [
+        'title' => 'این یک تایتل زیبا در مورد یک نوشته زیباست',
+        'text' => 'این یک تایتل زیبا در مورد یک نوشته زیباست این یک تایتل زیبا در مورد یک نوشته زیباست این یک تایتل زیبا در مورد یک نوشته زیباست این یک تایتل زیبا در مورد یک نوشته زیباست این یک تایتل زیبا در مورد یک نوشته زیباست این یک تایتل زیبا در مورد یک نوشته زیباست این یک تایتل زیبا در مورد یک نوشته زیباست این یک تایتل زیبا در مورد یک نوشته زیباست این یک تایتل زیبا در مورد یک نوشته زیباست این یک تایتل زیبا در مورد یک نوشته زیباست',
+
+    ];
+
+    /**
+     * @param array $data
+     * @return Article
+     * @throws \Throwable
+     */
+    protected function createNewArticleAndSaveItToDBOrFail(array $data)
+    {
+        $article = new Article($data);
+        $article->saveOrFail();
+        return $article;
+    }
 }
