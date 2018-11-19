449fdf946a9909ef71d4c0e9a2f7bb871a869739
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-11-19 10:18:21 +0330

    Create Content migration and enalble softDelete

---
+++ app/Containers/Content/Data/Migrations/2018_11_18_072307_create_contents_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateContentsTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('contents', function (Blueprint $table) {
+
+            $table->increments('id');
+            $table->timestamps();
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contents');
+    }
+}
