2724550aa81cf0681122588218ef5c18f3f9f2a0
Moslem Deris <moslem.deris@gmail.com>
2018-11-20 13:34:35 +0330

    Modify class to use DI and be testable

--- app/Containers/User/Actions/GetAllClientsAction.php
+++ app/Containers/User/Actions/GetAllClientsAction.php
@@ -2,29 +2,35 @@
 
 namespace App\Containers\User\Actions;
 
-use Apiato\Core\Foundation\Facades\Apiato;
+use App\Containers\User\Tasks\GetPaginatedAllUsersTask;
 use App\Ship\Parents\Actions\Action;
+use Illuminate\Pagination\LengthAwarePaginator;
 
-/**
- * Class GetAllClientsAction.
- *
- * @author Mahmoud Zalt <mahmoud@zalt.me>
- */
-class GetAllClientsAction extends Action
-{
+class GetAllClientsAction extends Action {
 
     /**
-     * @return  mixed
+     * @var GetPaginatedAllUsersTask
      */
-    public function run()
-    {
-        return Apiato::call('User@GetPaginatedAllUsersTask',
-            [],
-            [
-                'addRequestCriteria',
-                'clients',
-                'ordered',
-            ]
-        );
+    private $getPaginatedAllUsersTask;
+
+    /**
+     * GetAllClientsAction constructor.
+     *
+     * @param GetPaginatedAllUsersTask $getPaginatedAllUsersTask
+     */
+    public function __construct(GetPaginatedAllUsersTask $getPaginatedAllUsersTask) {
+        $this->getPaginatedAllUsersTask = $getPaginatedAllUsersTask;
+    }
+
+    /**
+     * @return LengthAwarePaginator
+     */
+    public function run(): LengthAwarePaginator {
+        $this->getPaginatedAllUsersTask->addRequestCriteria();
+        $this->getPaginatedAllUsersTask->clients();
+        $this->getPaginatedAllUsersTask->ordered();
+        $result = $this->getPaginatedAllUsersTask->run();
+
+        return $result;
     }
 }
