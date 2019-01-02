773573c65682c07ba32cfdd0ee193fe75b2ee391
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-01-03 02:13:38 +0330

    Add docs

--- app/Containers/Content/Actions/GetContentAction.php
+++ app/Containers/Content/Actions/GetContentAction.php
@@ -2,13 +2,22 @@
 
 namespace App\Containers\Content\Actions;
 
-use App\Ship\Parents\Actions\Action;
-use App\Ship\Parents\Requests\Request;
 use Apiato\Core\Foundation\Facades\Apiato;
+use App\Ship\Parents\Actions\Action;
 use App\Ship\Transporters\DataTransporter;
 
+/**
+ * Class GetContentAction
+ *
+ * @package App\Containers\Content\Actions
+ */
 class GetContentAction extends Action
 {
+    /**
+     * @param DataTransporter $transporter
+     *
+     * @return mixed
+     */
     public function run(DataTransporter $transporter)
     {
         $content = Apiato::call('Content@FindContentByIdTask', [$transporter->content_id]);
