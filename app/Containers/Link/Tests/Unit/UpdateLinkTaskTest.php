72c3d5cba1c557e39983e9094c10b105c0a0e30a
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-01-05 14:42:37 +0330

    Add Link unit test files

---
+++ app/Containers/Link/Tests/Unit/UpdateLinkTaskTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Containers\Link\Tests\Unit;
+
+use App\Containers\Link\Tests\TestCase;
+
+/**
+ * Class UpdateLinkTaskTest.
+ *
+ * @group link
+ * @group unit
+ */
+class UpdateLinkTaskTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function test_()
+    {
+        // create a data object
+        $data = [
+            // 'key' => 'value',
+        ];
+
+        /**
+         * you may want to do something like:
+         *
+         * 1) create a new Transporter with this data
+         * 2) create a specific Action or Task
+         * 3) pass the Transporter to the Action / Task
+         * 4) assert that everything is correct!
+         *
+         */
+
+        // assert something here
+        $this->assertEquals(true, true);
+    }
+}
