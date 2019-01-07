0a5bd83f04e318a495654d0f578e275a96e5952d
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-01-07 14:54:20 +0330

    Implement comment test

--- app/Containers/Comment/Tests/Unit/CreateCommentActionTest.php
+++ app/Containers/Comment/Tests/Unit/CreateCommentActionTest.php
@@ -2,7 +2,16 @@
 
 namespace App\Containers\Comment\Tests\Unit;
 
+use App\Containers\Authentication\Tasks\GetAuthenticatedUserTask;
+use App\Containers\Comment\Actions\CreateCommentAction;
+use App\Containers\Comment\Data\Transporters\CreateCommentTransporter;
+use App\Containers\Comment\Models\Comment;
+use App\Containers\Comment\Tasks\CreateCommentTask;
 use App\Containers\Comment\Tests\TestCase;
+use App\Containers\Content\Models\Content;
+use App\Containers\User\Models\User;
+use App\Ship\Transporters\DataTransporter;
+use PHPUnit\Framework\MockObject\MockObject;
 
 /**
  * Class CreateCommentActionTest.
@@ -12,28 +21,77 @@ use App\Containers\Comment\Tests\TestCase;
  */
 class CreateCommentActionTest extends TestCase
 {
-
+    /** @var CreateCommentAction $createCommentAction */
+    private $createCommentAction;
+    /** @var MockObject|CreateCommentTask mCreateCommentTask */
+    private $mCreateCommentTask;
+    /** @var array $dataForCreatingComment */
+    private $dataForCreatingComment;
+    /** @var Content $content */
+    private $content;
+    /** @var User $user */
+    private $user;
+    /** @var DataTransporter */
+    private $commentTransporter;
+    /** @var GetAuthenticatedUserTask $mGetAuthenticatedUserTask */
+    private $mGetAuthenticatedUserTask;
     /**
-     * @test
+     * @throws \Throwable
      */
-    public function test_()
+    public function setUp()
     {
-        // create a data object
-        $data = [
-            // 'key' => 'value',
+        parent::setUp();
+
+        $this->content = $this->createNewContentAndSaveItToDBOrFail();
+        $this->user = $this->createNewUserAndSaveItToDBOrFail();
+        // this data is for creating the comment
+        $this->dataForCreatingComment = [
+            'body' => TestCase::RAW_COMMENT_DATA['body'],
+            'content_id' => $this->content->id,
+            'user_id' => $this->user->id,
+            'parent_id' => 631,
         ];
+        $this->commentTransporter = new DataTransporter($this->dataForCreatingComment);
 
-        /**
-         * you may want to do something like:
-         *
-         * 1) create a new Transporter with this data
-         * 2) create a specific Action or Task
-         * 3) pass the Transporter to the Action / Task
-         * 4) assert that everything is correct!
-         *
-         */
+        $this->mCreateCommentTask = $this->getMockBuilder(CreateCommentTask::class)
+            ->setMethods(['run'])
+            ->disableOriginalConstructor()
+            ->getMock();
 
-        // assert something here
-        $this->assertEquals(true, true);
+        $this->mGetAuthenticatedUserTask = $this->getMockBuilder(GetAuthenticatedUserTask::class)
+            ->setMethods(['run'])
+            ->getMock();
+
+        $this->createCommentAction = new CreateCommentAction($this->mCreateCommentTask, $this->mGetAuthenticatedUserTask);
+    }
+
+    public function test_CreateCommentAndReturnAnCommentObject()
+    {
+        $this->mCreateCommentTask->expects($this->once())
+            ->method('run')
+            ->with($this->dataForCreatingComment)
+            ->willReturn(new Comment($this->dataForCreatingComment));
+
+        $this->mGetAuthenticatedUserTask->expects($this->once())
+            ->method('run')
+            ->willReturn($this->user);
+
+        /** @var DataTransporter $input */
+        $input = $this->commentTransporter;
+        $actual = $this->createCommentAction->run($input);
+        $expected = Comment::class;
+
+        $this->assertInstanceOf($expected, $actual, 'The returned object is not an instance of Comment.');
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        unset($this->createCommentAction);
+        unset($this->mCreateCommentTask);
+        unset($this->dataForCreatingComment);
+        unset($this->content);
+        unset($this->user);
+        unset($this->commentTransporter);
     }
 }
