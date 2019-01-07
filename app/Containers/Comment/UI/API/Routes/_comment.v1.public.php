cea092a4833980a75d118e00c0a1ab25b3fe31f1
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-01-07 14:52:21 +0330

    Update docs

--- app/Containers/Comment/UI/API/Routes/_comment.v1.public.php
+++ app/Containers/Comment/UI/API/Routes/_comment.v1.public.php
@@ -1,78 +1,31 @@
 <?php
 
 /**
- * @apiDefine ContentSuccessSingleResponse
+ * @apiDefine CommentSuccessSingleResponse
  * @apiSuccessExample {json} Success-Response:
 HTTP/1.1 200 OK
 {
     "data": {
-    "object": "Content",
-        "id": "7vmg6q36ak4b8kzr",
+    "object": "Comment",
+        "id": "qmv7dk48x5b690wx",
+        "body": "شسیشسی",
+        "content_id": "eq6am74064z0vpbn",
+        "user_id": "bml0wd39b5pkznag",
+        "parent_id": "qnwmkv5704blag6r",
         "created_at": {
-        "date": "2019-01-04 05:17:42.000000",
+        "date": "2019-01-07 14:46:30.000000",
             "timezone_type": 3,
             "timezone": "Asia/Tehran"
         },
         "updated_at": {
-        "date": "2019-01-04 05:17:42.000000",
+        "date": "2019-01-07 14:46:30.000000",
             "timezone_type": 3,
             "timezone": "Asia/Tehran"
-        },
-        "deleted_at": null,
-        "add-on": {
-        "article": {
-            "object": "Article",
-                "id": "ojl0865y0j4bgmew",
-                "title": "شسیشسی",
-                "text": "این متن یک نوشته است",
-                "content_id": "7vmg6q36ak4b8kzr",
-                "created_at": {
-                "date": "2019-01-04 05:17:42.000000",
-                    "timezone_type": 3,
-                    "timezone": "Asia/Tehran"
-                },
-                "updated_at": {
-                "date": "2019-01-04 05:17:42.000000",
-                    "timezone_type": 3,
-                    "timezone": "Asia/Tehran"
-                }
-            },
-            "repost": {
-            "object": "Repost",
-                "id": "kxeml73oyx4d9qbr",
-                "content_id": "7vmg6q36ak4b8kzr",
-                "referenced_content_id": "reloj65plp4v8ndy",
-                "created_at": {
-                "date": "2019-01-04 05:17:42.000000",
-                    "timezone_type": 3,
-                    "timezone": "Asia/Tehran"
-                },
-                "updated_at": {
-                "date": "2019-01-04 05:17:42.000000",
-                    "timezone_type": 3,
-                    "timezone": "Asia/Tehran"
-                }
-            },
-            "link": {
-            "object": "Link",
-                "id": "dqb9073ap3ekzgrm",
-                "link_url": "https://stackoverflow.com/questions/38726530/replace-snake-case-to-camelcase-in-part-of-a-string",
-                "content_id": "7vmg6q36ak4b8kzr",
-                "created_at": {
-                "date": "2019-01-04 05:17:42.000000",
-                    "timezone_type": 3,
-                    "timezone": "Asia/Tehran"
-                },
-                "updated_at": {
-                "date": "2019-01-04 05:17:42.000000",
-                    "timezone_type": 3,
-                    "timezone": "Asia/Tehran"
-                }
-            }
         }
     },
     "meta": {
     "include": [],
         "custom": []
     }
-} */
+}
+*/
