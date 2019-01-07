917ebdd540cd8b424017a0485b0534a9dc1d2e5e
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-01-07 09:54:48 +0330

    update test case

--- app/Containers/Comment/Tests/TestCase.php
+++ app/Containers/Comment/Tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace App\Containers\Comment\Tests;
 
+use App\Containers\Comment\Models\Comment;
 use App\Ship\Parents\Tests\PhpUnit\TestCase as ShipTestCase;
 
 /**
@@ -11,5 +12,20 @@ use App\Ship\Parents\Tests\PhpUnit\TestCase as ShipTestCase;
  */
 class TestCase extends ShipTestCase
 {
-    // ..
+    protected const RAW_COMMENT_DATA = [
+        'body' => 'کامنتی زیبا برای اپی زیبا',
+    ];
+
+    /**
+     * @param array $data
+     *
+     * @return Comment
+     * @throws \Throwable
+     */
+    protected function createNewCommentAndSaveItToDBOrFail()
+    {
+        $comment = new Comment();
+        $comment->saveOrFail();
+        return $comment;
+    }
 }
