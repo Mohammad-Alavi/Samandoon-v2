527956c80a584007a687ecec490676c11ec57c12
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-01-07 14:53:18 +0330

    Add comment controller

---
+++ app/Containers/Comment/UI/API/Controllers/Controller.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Containers\Comment\UI\API\Controllers;
+
+use Apiato\Core\Foundation\Facades\Apiato;
+use App\Containers\Comment\UI\API\Requests\CreateCommentRequest;
+use App\Containers\Comment\UI\API\Transformers\CommentTransformer;
+use App\Ship\Parents\Controllers\ApiController;
+use App\Ship\Transporters\DataTransporter;
+
+/**
+ * Class Controller
+ *
+ * @package App\Containers\Comment\UI\API\Controllers
+ */
+class Controller extends ApiController
+{
+    /**
+     * @param CreateCommentRequest $request
+     *
+     * @return array
+     */
+    public function createComment(CreateCommentRequest $request)
+    {
+        $comment = Apiato::call('Comment@CreateCommentAction', [new DataTransporter($request)]);
+        return $this->transform($comment, CommentTransformer::class);
+    }
+}
