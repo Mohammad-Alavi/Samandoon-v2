622137fe6d6158358e1d6a65a8d8b806900e178d
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-02-06 14:11:32 +0330

    add user to comment transformer

--- app/Containers/Comment/UI/API/Transformers/CommentTransformer.php
+++ app/Containers/Comment/UI/API/Transformers/CommentTransformer.php
@@ -3,6 +3,7 @@
 namespace App\Containers\Comment\UI\API\Transformers;
 
 use App\Containers\Comment\Models\Comment;
+use App\Containers\User\UI\API\Transformers\UserTransformer;
 use App\Ship\Parents\Transformers\Transformer;
 use Vinkla\Hashids\Facades\Hashids;
 
@@ -29,16 +30,18 @@ class CommentTransformer extends Transformer
      */
     public function transform(Comment $entity)
     {
+        /** @var UserTransformer $userTransformer */
+        $userTransformer = new UserTransformer();
+
         $response = [
             'object' => 'Comment',
             'id' => $entity->getHashedKey(),
             'body' => $entity->body,
             'content_id' => Hashids::encode($entity->content_id),
-            'user_id' => Hashids::encode($entity->user_id),
             'parent_id' => $entity->parent_id ? Hashids::encode($entity->parent_id) : null,
             'created_at' => $entity->created_at,
             'updated_at' => $entity->updated_at,
-
+            'user' => $userTransformer->transform($entity->user),
         ];
 
         $response = $this->ifAdmin([
