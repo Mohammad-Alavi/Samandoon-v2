5d924e265f407ea591343cdbbb9d1a03394ee3ca
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-02-18 02:22:49 +0330

    update docs to include subject addon

--- app/Containers/Content/UI/API/Routes/_content.v1.public.php
+++ app/Containers/Content/UI/API/Routes/_content.v1.public.php
@@ -7,30 +7,30 @@ HTTP/1.1 200 OK
 {
     "data": {
     "object": "Content",
-    "id": "qmv7dk48x5b690wx",
+    "id": "lnmojg5bwy5ew80r",
     "created_at": {
-        "date": "2019-02-11 05:27:22.000000",
+        "date": "2019-02-17 18:15:29.000000",
       "timezone_type": 3,
       "timezone": "Asia\/Tehran"
     },
     "updated_at": {
-        "date": "2019-02-11 05:27:22.000000",
+        "date": "2019-02-17 18:15:29.000000",
       "timezone_type": 3,
       "timezone": "Asia\/Tehran"
     },
     "add-on": {
         "article": {
             "object": "Article",
-        "id": "qmv7dk48x5b690wx",
-        "text": "این برای حذف است",
-        "content_id": "qmv7dk48x5b690wx",
+        "id": "7ozb8r4zvn369anp",
+        "text": "ای برای ریختن پشم است",
+        "content_id": "lnmojg5bwy5ew80r",
         "created_at": {
-                "date": "2019-02-11 05:27:22.000000",
+                "date": "2019-02-17 18:15:29.000000",
           "timezone_type": 3,
           "timezone": "Asia\/Tehran"
         },
         "updated_at": {
-                "date": "2019-02-11 05:27:22.000000",
+                "date": "2019-02-17 18:15:29.000000",
           "timezone_type": 3,
           "timezone": "Asia\/Tehran"
         }
@@ -38,35 +38,64 @@ HTTP/1.1 200 OK
       "repost": null,
       "link": {
             "object": "Link",
-        "id": "qmv7dk48x5b690wx",
+        "id": "7ozb8r4zvn369anp",
         "link_url": "https:\/\/stackoverflow.com",
-        "content_id": "qmv7dk48x5b690wx",
+        "content_id": "lnmojg5bwy5ew80r",
         "created_at": {
-                "date": "2019-02-11 05:27:22.000000",
+                "date": "2019-02-17 18:15:29.000000",
           "timezone_type": 3,
           "timezone": "Asia\/Tehran"
         },
         "updated_at": {
-                "date": "2019-02-11 05:27:22.000000",
+                "date": "2019-02-17 18:15:29.000000",
           "timezone_type": 3,
           "timezone": "Asia\/Tehran"
         }
       },
-      "image": null
+      "image": {
+            "object": "Image",
+        "id": "8gq07y4nd03wkxvo",
+        "image_url": "http:\/\/api.samandoon.local\/v1\/storage\/87\/516912ff6e44036f6ea62c0c5cf577a0.jpg",
+        "created_at": {
+                "date": "2019-02-17 18:15:29.000000",
+          "timezone_type": 3,
+          "timezone": "Asia\/Tehran"
+        },
+        "updated_at": {
+                "date": "2019-02-17 18:15:29.000000",
+          "timezone_type": 3,
+          "timezone": "Asia\/Tehran"
+        }
+      },
+      "subject": {
+            "object": "Subject",
+        "id": "qovgxe3xm4ladbpm",
+        "subject": "بدون موضوع",
+        "created_at": {
+                "date": "2019-02-17 18:15:29.000000",
+          "timezone_type": 3,
+          "timezone": "Asia\/Tehran"
+        },
+        "updated_at": {
+                "date": "2019-02-17 18:15:29.000000",
+          "timezone_type": 3,
+          "timezone": "Asia\/Tehran"
+        }
+      }
     },
     "stats": {
-        "like_count": 1,
+        "like_count": 0,
       "liked_by_current_user": false,
-      "comment_count": 2
+      "comment_count": 0
     },
     "user": {
         "object": "User",
-      "id": "bml0wd39b5pkznag",
+      "id": "qmv7dk48x5b690wx",
       "first_name": null,
       "last_name": null,
       "nick_name": null,
       "email": null,
-      "phone": "+989169302582",
+      "phone": "+989391079907",
       "is_phone_confirmed": true,
       "is_email_confirmed": false,
       "gender": null,
@@ -83,23 +112,23 @@ HTTP/1.1 200 OK
         "avatar_thumb": "http:\/\/api.samandoon.local\/v1\/storage\/default_images\/avatar_thumb.png"
       },
       "stats": {
-            "followings_count": 0,
+            "followings_count": 1,
         "followers_count": 1,
-        "is_following": false,
-        "content_count": 4
+        "followed_by_current_user": false,
+        "content_count": 53
       },
       "created_at": {
-            "date": "2019-02-11 03:14:09.000000",
+            "date": "2019-02-11 03:06:14.000000",
         "timezone_type": 3,
         "timezone": "Asia\/Tehran"
       },
       "updated_at": {
-            "date": "2019-02-11 03:14:30.000000",
+            "date": "2019-02-12 03:59:00.000000",
         "timezone_type": 3,
         "timezone": "Asia\/Tehran"
       },
-      "readable_created_at": "13 hours ago",
-      "readable_updated_at": "13 hours ago"
+      "readable_created_at": "6 days ago",
+      "readable_updated_at": "5 days ago"
     }
   },
   "meta": {
@@ -107,4 +136,4 @@ HTTP/1.1 200 OK
     "custom": []
   }
 }
- */
+*/
--- public/api/private/documentation/api_project.js
+++ public/api/private/documentation/api_project.js
@@ -18,7 +18,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-02-12T01:57:36.720Z",
+    "time": "2019-02-17T15:13:50.818Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
