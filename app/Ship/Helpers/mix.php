2e336927018b94b7a695d8edaf7e4991dd8017f2
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-02-18 05:16:47 +0330

    add converter class

--- app/Ship/Helpers/mix.php
+++ app/Ship/Helpers/mix.php
@@ -1,3 +1,33 @@
 <?php
 
 // Add your helper functions here...
+namespace App\Ship\Helpers;
+
+class ArabicToPersianStringConverter
+{
+    public static function Convert($string)
+    {
+        $characters = [
+            'ك' => 'ک',
+            'دِ' => 'د',
+            'بِ' => 'ب',
+            'زِ' => 'ز',
+            'ذِ' => 'ذ',
+            'شِ' => 'ش',
+            'سِ' => 'س',
+            'ى' => 'ی',
+            'ي' => 'ی',
+            '١' => '۱',
+            '٢' => '۲',
+            '٣' => '۳',
+            '٤' => '۴',
+            '٥' => '۵',
+            '٦' => '۶',
+            '٧' => '۷',
+            '٨' => '۸',
+            '٩' => '۹',
+            '٠' => '۰',
+        ];
+        return str_replace(array_keys($characters), array_values($characters), $string);
+    }
+}
