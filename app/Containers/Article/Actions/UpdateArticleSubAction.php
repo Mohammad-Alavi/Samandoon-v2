6718e44f7c8b3d68d404f621314f025c162dae63
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-02-21 05:14:28 +0330

    sync content tags with that whicha re included in article text

--- app/Containers/Article/Actions/UpdateArticleSubAction.php
+++ app/Containers/Article/Actions/UpdateArticleSubAction.php
@@ -4,6 +4,9 @@ namespace App\Containers\Article\Actions;
 
 use App\Containers\Article\Models\Article;
 use App\Containers\Article\Tasks\UpdateArticleTask;
+use App\Containers\Content\Models\Content;
+use App\Containers\Content\Tasks\ExtractHashtagsFromStringTask;
+use App\Containers\Content\Tasks\FindContentByIdTask;
 use App\Ship\Parents\Actions\SubAction;
 
 /**
@@ -13,17 +16,24 @@ use App\Ship\Parents\Actions\SubAction;
  */
 class UpdateArticleSubAction extends SubAction
 {
-
+    /** @var UpdateArticleTask $updateArticleTask */
     protected $updateArticleTask;
-
+    /** @var ExtractHashtagsFromStringTask $extractHashtagsFromStringTask */
+    protected $extractHashtagsFromStringTask;
+    /** @var FindContentByIdTask $findContentByIdTask */
+    protected $findContentByIdTask;
     /**
      * UpdateArticleSubAction constructor.
      *
      * @param UpdateArticleTask $updateArticleTask
      */
-    public function __construct(UpdateArticleTask $updateArticleTask)
+    public function __construct(UpdateArticleTask $updateArticleTask,
+                                ExtractHashtagsFromStringTask $extractHashtagsFromStringTask,
+                                FindContentByIdTask $findContentByIdTask)
     {
         $this->updateArticleTask = $updateArticleTask;
+        $this->extractHashtagsFromStringTask = $extractHashtagsFromStringTask;
+        $this->findContentByIdTask = $findContentByIdTask;
     }
 
     /**
@@ -35,8 +45,15 @@ class UpdateArticleSubAction extends SubAction
      */
     public function run(array $data, $id): Article
     {
-
         $article = $this->updateArticleTask->run($id, $data);
+
+        /** @var array $articleTags */
+        $articleTags = $this->extractHashtagsFromStringTask->run($data['text']);
+        /** @var Content $content */
+        $content = $this->findContentByIdTask->run($id);
+
+        //attach tags that are in article to content
+        $content->syncTags($articleTags);
 
         return $article;
     }
