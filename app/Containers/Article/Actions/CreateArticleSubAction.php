89ac6b827a13e4d2e0298ba66d5f5da93c8df1f1
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-02-21 05:01:37 +0330

    now adds the included hashtags available in article's text to content

--- app/Containers/Article/Actions/CreateArticleSubAction.php
+++ app/Containers/Article/Actions/CreateArticleSubAction.php
@@ -4,6 +4,9 @@ namespace App\Containers\Article\Actions;
 
 use App\Containers\Article\Models\Article;
 use App\Containers\Article\Tasks\CreateArticleTask;
+use App\Containers\Content\Models\Content;
+use App\Containers\Content\Tasks\ExtractHashtagsFromStringTask;
+use App\Containers\Content\Tasks\FindContentByIdTask;
 use App\Ship\Parents\Actions\SubAction;
 
 /**
@@ -14,19 +17,25 @@ use App\Ship\Parents\Actions\SubAction;
 class CreateArticleSubAction extends SubAction
 {
 
-    /**
-     * @var CreateArticleTask $createArticleTask
-     */
+    /** @var CreateArticleTask $createArticleTask */
     protected $createArticleTask;
+    /** @var ExtractHashtagsFromStringTask $extractHashtagsFromStringTask */
+    protected $extractHashtagsFromStringTask;
+    /** @var FindContentByIdTask $findContentByIdTask */
+    protected $findContentByIdTask;
 
     /**
      * CreateArticleSubAction constructor.
      *
      * @param CreateArticleTask $createArticleTask
      */
-    public function __construct(CreateArticleTask $createArticleTask)
+    public function __construct(CreateArticleTask $createArticleTask,
+                                ExtractHashtagsFromStringTask $extractHashtagsFromStringTask,
+                                FindContentByIdTask $findContentByIdTask)
     {
         $this->createArticleTask = $createArticleTask;
+        $this->extractHashtagsFromStringTask = $extractHashtagsFromStringTask;
+        $this->findContentByIdTask = $findContentByIdTask;
     }
 
     /**
@@ -42,7 +51,17 @@ class CreateArticleSubAction extends SubAction
             'text' => $data['text'],
             'content_id' => $content_id,
         ];
+
+        /** @var Article $article */
         $article = $this->createArticleTask->run($articleData);
+
+        /** @var array $articleTags */
+        $articleTags = $this->extractHashtagsFromStringTask->run($articleData['text']);
+        /** @var Content $content */
+        $content = $this->findContentByIdTask->run($articleData['content_id']);
+
+        //attach tags that are in article to content
+        $content->attachTags($articleTags);
 
         return $article;
     }
