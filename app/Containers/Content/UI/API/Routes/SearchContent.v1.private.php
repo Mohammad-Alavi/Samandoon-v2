0c176b6188d418cc1cfdebe66c5e1fe74ccbd6bb
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-02-22 11:07:51 +0330

    doc update

--- app/Containers/Content/UI/API/Routes/SearchContent.v1.private.php
+++ app/Containers/Content/UI/API/Routes/SearchContent.v1.private.php
@@ -1,5 +1,6 @@
 <?php
 
+
 /**
  * @apiGroup           Search
  * @apiName            searchContent
@@ -10,10 +11,12 @@
  * @apiVersion         1.0.0
  * @apiPermission      Authenticated
  *
+ * @apiParam           {string} [tag] this and tag_type parameter should be send together
+ * @apiParam           {string=subject,content} [tag_type] this and tag parameter should be send together
+
  * @apiUse             ContentSuccessSingleResponse
  *
  */
-
 /** @var Route $router */
 $router->get('search/content', [
     'as' => 'api_content_search_content',
