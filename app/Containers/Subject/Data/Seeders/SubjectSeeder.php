f7ee58f3a3f7b9943d94e06dc3ec94dad101c921
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-02-27 16:17:57 +0330

    add subject seeder

--- app/Containers/Subject/Data/Seeders/SubjectSeeder.php
+++ app/Containers/Subject/Data/Seeders/SubjectSeeder.php
@@ -3,11 +3,33 @@
 namespace App\Containers\Subject\Data\Seeders;
 
 use App\Ship\Parents\Seeders\Seeder;
+use Carbon\Carbon;
+use DB;
 
 class SubjectSeeder extends Seeder
 {
     public function run()
     {
-        // TODO Implement SubjectSeeder
+        // automatically add subject from subject-container config file
+        $subjectArray = [];
+        $iterator = 0;
+        foreach (config('subject-container.subjects') as $key => $value) {
+            array_push($subjectArray, [
+                'id' => $iterator + 1,
+                'name' => json_encode([
+                    'en' => $value,
+                ]),
+                'order_column' => $iterator + 1,
+                'slug' => json_encode([
+                    'en' => $key,
+                ]),
+                'type' => 'subject',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+            $iterator++;
+        }
+
+        DB::table('tags')->insert($subjectArray);
     }
 }
