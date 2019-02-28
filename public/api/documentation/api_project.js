fe8bf7a6c3686d46b8c728f9424ead11c3ed7ec5
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-02-28 15:45:42 +0330

    update docs

--- public/api/documentation/api_project.js
+++ public/api/documentation/api_project.js
@@ -18,7 +18,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-02-28T12:14:47.067Z",
+    "time": "2019-02-28T12:15:37.345Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
