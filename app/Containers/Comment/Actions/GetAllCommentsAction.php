74d3f93630d779611f7cabc8ac9ea19521dfa036
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-03-30 19:02:34 +0430

    remove unued codes

--- app/Containers/Comment/Actions/GetAllCommentsAction.php
+++ app/Containers/Comment/Actions/GetAllCommentsAction.php
@@ -2,9 +2,8 @@
 
 namespace App\Containers\Comment\Actions;
 
-use App\Ship\Parents\Actions\Action;
-use App\Ship\Parents\Requests\Request;
 use Apiato\Core\Foundation\Facades\Apiato;
+use App\Ship\Parents\Actions\Action;
 use App\Ship\Transporters\DataTransporter;
 
 class GetAllCommentsAction extends Action
@@ -17,7 +16,7 @@ class GetAllCommentsAction extends Action
     public function run(DataTransporter $transporter)
     {
         $content = Apiato::call('Content@FindContentByIdTask', [$transporter->content_id]);
-        $comments = Apiato::call('Comment@GetAllCommentsTask', [$content->id], ['OrderByCreationDateDescendingCriteria']);
+        $comments = Apiato::call('Comment@GetAllCommentsTask', [$content->id]);
         return $comments;
     }
 }
