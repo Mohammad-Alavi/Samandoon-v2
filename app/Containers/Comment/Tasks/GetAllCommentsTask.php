a5e5ef965884f92abe1e2f048ddd6f82313aaa86
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-03-31 04:56:34 +0430

    get comment response now has pagination

--- app/Containers/Comment/Tasks/GetAllCommentsTask.php
+++ app/Containers/Comment/Tasks/GetAllCommentsTask.php
@@ -2,7 +2,9 @@
 
 namespace App\Containers\Comment\Tasks;
 
+use App\Containers\Comment\Data\Criterias\ThisContentCriteria;
 use App\Containers\Comment\Data\Repositories\CommentRepository;
+use App\Ship\Criterias\Eloquent\OrderByCreationDateDescendingCriteria;
 use App\Ship\Parents\Tasks\Task;
 
 class GetAllCommentsTask extends Task
@@ -27,7 +29,8 @@ class GetAllCommentsTask extends Task
      */
     public function run(string $content_id)
     {
-        return $this->repository->findByContentId($content_id);
-
+        $this->repository->pushCriteria(new ThisContentCriteria($content_id));
+        $this->repository->pushCriteria(new OrderByCreationDateDescendingCriteria());
+        return $this->repository->paginate();
     }
 }
