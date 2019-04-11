5521feeb5acecb912791f82002e91c1d65013c12
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-04-12 02:35:59 +0430

    fix get all comments

--- app/Containers/Comment/Tasks/GetAllCommentsTask.php
+++ app/Containers/Comment/Tasks/GetAllCommentsTask.php
@@ -3,7 +3,7 @@
 namespace App\Containers\Comment\Tasks;
 
 use App\Containers\Comment\Data\Repositories\CommentRepository;
-use App\Containers\Content\Data\Criterias\ThisFCMTokensCriteria;
+use App\Containers\Content\Data\Criterias\ThisContentCriteria;
 use App\Ship\Criterias\Eloquent\OrderByCreationDateDescendingCriteria;
 use App\Ship\Parents\Tasks\Task;
 use Prettus\Repository\Exceptions\RepositoryException;
@@ -32,7 +32,7 @@ class GetAllCommentsTask extends Task
      */
     public function run(string $content_id)
     {
-        $this->repository->pushCriteria(new ThisFCMTokensCriteria($content_id));
+        $this->repository->pushCriteria(new ThisContentCriteria($content_id));
         $this->repository->pushCriteria(new OrderByCreationDateDescendingCriteria());
         return $this->repository->paginate();
     }
