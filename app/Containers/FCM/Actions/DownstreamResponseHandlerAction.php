0642e58f0b02a98297f695e051eeda00a98da110
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-04-11 08:24:40 +0430

    disable downstream handler untill fully implemented and tested

--- app/Containers/FCM/Actions/DownstreamResponseHandlerAction.php
+++ app/Containers/FCM/Actions/DownstreamResponseHandlerAction.php
@@ -2,21 +2,24 @@
 
 namespace App\Containers\FCM\Actions;
 
-use App\Ship\Parents\Actions\SubAction;
 use Apiato\Core\Foundation\Facades\Apiato;
+use App\Containers\FCM\Exceptions\CouldNotHandleDownstreamResponseException;
+use App\Ship\Parents\Actions\SubAction;
 use App\Ship\Parents\Exceptions\Exception;
-use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class DownstreamResponseHandlerAction extends SubAction
 {
+    /**
+     * @param $downstreamResponse
+     */
     public function run($downstreamResponse)
     {
         try {
-            Apiato::call('FCM@DeleteFCMTokenTask', [$downstreamResponse->tokensToDelete()]);
-            Apiato::call('FCM@UpdateFCMTokenTask', [$downstreamResponse->tokensToModify()]);
+//            Apiato::call('FCM@DeleteFCMTokenTask', [$downstreamResponse->tokensToDelete()]);
+//            Apiato::call('FCM@UpdateFCMTokenTask', [$downstreamResponse->tokensToModify()]);
 //         Apiato::call('FCM@ResendFCMTokenTask', [$downstreamResponse->tokensToRetry()]); // TODO implemend resend fcm token task
         } catch (Exception $exception) {
-            throw new ProcessFailedException($exception->getMessage());
+            throw new CouldNotHandleDownstreamResponseException($exception->getMessage());
         }
     }
 }
