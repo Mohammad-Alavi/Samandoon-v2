874c57153c863df9c3bd5a85e2f1aa5b383265d5
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-04-11 08:00:46 +0430

    modify user follow notification to use new fcm pkg

--- app/Containers/User/Actions/FollowAction.php
+++ app/Containers/User/Actions/FollowAction.php
@@ -3,16 +3,12 @@
 namespace App\Containers\User\Actions;
 
 use Apiato\Core\Foundation\Facades\Apiato;
-use App\Containers\FCM\Models\FCMToken;
+use App\Containers\FCM\Notifications\FCMChannel;
 use App\Containers\User\Models\User;
 use App\Containers\User\Notifications\UserFollowedNotification;
 use App\Ship\Parents\Actions\Action;
 use App\Ship\Transporters\DataTransporter;
 use Illuminate\Validation\UnauthorizedException;
-use LaravelFCM\Facades\FCM;
-use LaravelFCM\Message\OptionsBuilder;
-use LaravelFCM\Message\PayloadDataBuilder;
-use LaravelFCM\Message\PayloadNotificationBuilder;
 
 class FollowAction extends Action
 {
@@ -34,44 +30,8 @@ class FollowAction extends Action
 
         $result = Apiato::call('User@FollowTask', [$AuthenticatedUser, $targetUser]);
 
-        // send notification
-        $targetUser->notifyNow(new UserFollowedNotification($AuthenticatedUser), ['database']);
-
-        $optionBuilder = new OptionsBuilder();
-        $optionBuilder->setTimeToLive(60 * 20);
-
-        $notificationBuilder = new PayloadNotificationBuilder('سمندون');
-        $notificationBuilder->setBody('[' . $AuthenticatedUser->nick_name . ']' . ' شما را دنبال کرد')
-            ->setSound('default');
-
-        $dataBuilder = new PayloadDataBuilder();
-        $dataBuilder->addData(['a_data' => 'my_data']);
-
-        $option = $optionBuilder->build();
-        $notification = $notificationBuilder->build();
-        $data = $dataBuilder->build();
-
-        $tokens = FCMToken::where('user_id', $targetUser->id)->pluck('android_fcm_token')->toArray();
-        if (!empty($tokens)) {
-
-            $downstreamResponse = FCM::sendTo($tokens, $option, $notification, $data);
-
-            $downstreamResponse->numberSuccess();
-            $downstreamResponse->numberFailure();
-            $downstreamResponse->numberModification();
-
-            //return Array - you must remove all this tokens in your database
-            $downstreamResponse->tokensToDelete();
-
-            //return Array (key : oldToken, value : new token - you must change the token in your database )
-            $downstreamResponse->tokensToModify();
-
-            //return Array - you should try to resend the message to the tokens in the array
-            $downstreamResponse->tokensToRetry();
-
-            // return Array (key:token, value:errror) - in production you should remove from your database the tokens
-        }
-
+        // send/save notification to database and send to FCM
+        $targetUser->notifyNow(new UserFollowedNotification($AuthenticatedUser), [FCMChannel::class, 'database']);
 
         return $result;
     }
--- app/Containers/User/Notifications/UserFollowedNotification.php
+++ app/Containers/User/Notifications/UserFollowedNotification.php
@@ -2,8 +2,14 @@
 
 namespace App\Containers\User\Notifications;
 
+use App\Containers\FCM\Actions\DownstreamResponseHandlerAction;
+use App\Containers\FCM\Tasks\GetAllUserFCMTokensTask;
 use App\Ship\Parents\Notifications\Notification;
-use Benwilkins\FCM\FcmMessage;
+use Illuminate\Support\Facades\App;
+use LaravelFCM\Facades\FCM;
+use LaravelFCM\Message\OptionsBuilder;
+use LaravelFCM\Message\PayloadDataBuilder;
+use LaravelFCM\Message\PayloadNotificationBuilder;
 
 /**
  * Class UserFollowedNotification
@@ -12,14 +18,56 @@ class UserFollowedNotification extends Notification
 {
     protected $doer;
 
+    /**
+     * UserFollowedNotification constructor.
+     *
+     * @param $doer
+     */
     public function __construct($doer)
     {
         $this->doer = $doer;
     }
 
-    public function via($notifiable)
+    /**
+     * @param $notifiable
+     */
+    public function toFCM($notifiable)
     {
-        return ['database'];
+        // set fcm options
+        $optionBuilder = new OptionsBuilder();
+        $optionBuilder->setTimeToLive(60 * 20);
+
+        // create notification payload
+        $notificationBuilder = new PayloadNotificationBuilder('سمندون');
+        $notificationBuilder->setBody('[' . $this->doer->nick_name . ']' . ' شما را دنبال کرد')
+            ->setSound('default');
+
+        // create data payload
+        $dataBuilder = new PayloadDataBuilder();
+        $dataBuilder->addData(['a_data' => 'my_data']);
+
+        // build those top three ^
+        $option = $optionBuilder->build();
+        $notification = $notificationBuilder->build();
+        $data = $dataBuilder->build();
+
+        // get all user fcm tokens
+        /** @var GetAllUserFCMTokensTask $getAllUserFCMTokensTask */
+        $getAllUserFCMTokensTask = App::make(GetAllUserFCMTokensTask::class);
+        $tokens = $getAllUserFCMTokensTask->run($notifiable->id);
+
+        // if user have any fcm token then send the notification to them
+        if (!empty($tokens)) {
+            // send notification to fcm
+            /** @var FCM $downstreamResponse */
+            $downstreamResponse = FCM::sendTo($tokens, $option, $notification, $data);
+
+            // handle the downstream response: errors, modified tokens, invalid tokens, etc...
+            /** @var DownstreamResponseHandlerAction $downstreamResponseHandlerAction */
+            $downstreamResponseHandlerAction = App::make(DownstreamResponseHandlerAction::class);
+            $downstreamResponseHandlerAction->run($downstreamResponse);
+        }
+
     }
 
     public function toDatabase($notifiable)
