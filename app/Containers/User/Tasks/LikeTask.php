f3aac04c18726da3c5a2db65bcae0a7e66b99217
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-04-11 18:57:46 +0430

    use like content task

--- app/Containers/User/Tasks/LikeTask.php
+++ app/Containers/User/Tasks/LikeTask.php
@@ -3,7 +3,10 @@
 namespace App\Containers\User\Tasks;
 
 use Apiato\Core\Abstracts\Exceptions\Exception;
+use App\Containers\Comment\Notifications\CommentLikedNotification;
 use App\Containers\Content\Models\Content;
+use App\Containers\Content\Notifications\ContentLikedNotification;
+use App\Containers\FCM\Notifications\FCMChannel;
 use App\Containers\User\Models\User;
 use App\Ship\Exceptions\UpdateResourceFailedException;
 use App\Ship\Parents\Tasks\Task;
@@ -22,19 +25,23 @@ class LikeTask extends Task
     {
         try {
             DB::beginTransaction();
-            if (!$is_liked = $user->hasLiked($content)) {
+            if (!$is_new_like = $user->hasLiked($content)) {
                 $user->like($content);
-                $is_liked = true;
-                if ($user->id == $content->user->id) {
-                    // TODO DON'T NOTIFY USER IF HE LIKE HIS OWN CONTENT
-                }
+                $is_new_like = true;
+            }
+            else {
+                $is_new_like = false;
             }
         } catch (Exception $exception) {
             DB::rollBack();
             throw new UpdateResourceFailedException('Failed to like the specified resource');
         }
         DB::commit();
-
+        // only send notification if someone beside the owner liked the resource
+        if ($is_new_like && $user->id != $content->user->id) {
+            // send/save notification to database and send to FCM
+            $content->user->notifyNow(new ContentLikedNotification($user, $content), [FCMChannel::class, 'database']);
+        }
         return $content->refresh();
     }
 }
