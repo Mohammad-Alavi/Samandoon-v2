3a4e788636e8fdf928b3d8f7c8bad32ddd8654e3
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-04-12 05:07:33 +0430

    hotfix remove unused parameter

--- app/Containers/NotificationCenter/Tasks/GetNotificationsTask.php
+++ app/Containers/NotificationCenter/Tasks/GetNotificationsTask.php
@@ -7,7 +7,7 @@ use App\Ship\Parents\Tasks\Task;
 
 class GetNotificationsTask extends Task
 {
-    public function run($limit, User $user)
+    public function run(User $user)
     {
         $unreadNotifications = $user->unreadNotifications()->paginate(100);
         $user->notifications->markAsRead();
