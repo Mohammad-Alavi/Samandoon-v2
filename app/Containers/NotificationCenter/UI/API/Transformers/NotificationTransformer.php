6449e430db1e649f560790f74c33d06e0108ceb0
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-04-18 05:22:50 +0430

    now return compelete user transformer instead of just doer id

--- app/Containers/NotificationCenter/UI/API/Transformers/NotificationTransformer.php
+++ app/Containers/NotificationCenter/UI/API/Transformers/NotificationTransformer.php
@@ -2,20 +2,29 @@
 
 namespace App\Containers\NotificationCenter\UI\API\Transformers;
 
+use App\Containers\User\Models\User;
+use App\Containers\User\UI\API\Transformers\UserTransformer;
 use Vinkla\Hashids\Facades\Hashids;
 
 class NotificationTransformer
 {
+    /**
+     * @param $notifications
+     *
+     * @return array
+     */
     public function transform($notifications)
     {
+        $userTransformer = new UserTransformer();
         $tempArray = [];
 
         foreach ($notifications as $notification) {
             $response = [
                 'id' => $notification->id,
                 'type' => $notification->type,
-                'doer_id' => array_key_exists('doer_id', $notification->data) ? Hashids::encode($notification->data['doer_id']) : null,
-                'doer_name' => array_key_exists('doer_name', $notification->data) ? $notification->data['doer_name'] : null,
+                'user' => array_key_exists('doer_id', $notification->data) ?
+                    $userTransformer->transform(User::where('id', '=', $notification->data['doer_id'])->first()) : null,
+//                'doer_name' => array_key_exists('doer_name', $notification->data) ? $notification->data['doer_name'] : null,
                 'object_id' => array_key_exists('object_id', $notification->data) ? Hashids::encode($notification->data['object_id']) : null,
                 'object_text' => array_key_exists('object_text', $notification->data) ? $notification->data['object_text'] : null,
                 'read_at' => $notification->read_at,
@@ -37,8 +46,8 @@ class NotificationTransformer
                     'current_page' => $paginatedDataArray['current_page'],
                     'total_pages' => $paginatedDataArray['last_page'],
                     'links' => [],
-                ]
-            ]
+                ],
+            ],
         ];
 
         return $data;
