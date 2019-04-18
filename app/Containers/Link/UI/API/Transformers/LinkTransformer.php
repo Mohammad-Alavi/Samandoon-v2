b3aa605d6cd0cd651141a1d9e499ef43679e6f37
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-04-18 07:10:20 +0430

    remove unused content_id

--- app/Containers/Link/UI/API/Transformers/LinkTransformer.php
+++ app/Containers/Link/UI/API/Transformers/LinkTransformer.php
@@ -4,7 +4,6 @@ namespace App\Containers\Link\UI\API\Transformers;
 
 use App\Containers\Link\Models\Link;
 use App\Ship\Parents\Transformers\Transformer;
-use Vinkla\Hashids\Facades\Hashids;
 
 /**
  * Class LinkTransformer
@@ -38,14 +37,13 @@ class LinkTransformer extends Transformer
             'object' => 'Link',
             'id' => $entity->getHashedKey(),
             'link_url' => $entity->link_url,
-            'content_id' => Hashids::encode($entity->content_id),
             'created_at' => $entity->created_at,
             'updated_at' => $entity->updated_at,
         ];
 
         $response = $this->ifAdmin([
             'real_id' => $entity->id,
-             'deleted_at' => $entity->deleted_at,
+            'deleted_at' => $entity->deleted_at,
         ], $response);
 
         return $response;
